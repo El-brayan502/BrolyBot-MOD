f460592892ec06ab2b966b2af220ca3fd97240bf
ɮʀǟʏǟռ-ɢȶ-ɨռֆǟռօ <brayanphonkponk@gmail.com>
2024-10-19 00:51:15 -0600

    Actualizar bot-audios.js

--- plugins/bot-audios.js
+++ plugins/bot-audios.js
@@ -29,7 +29,7 @@ let audioMsg = {
 'Tengo los calzones': 'https://qu.ax/pzRp.mp3',
 'Calla Fan de bts': 'https://qu.ax/oqNf.mp3',
     'hola': 'https://qu.ax/oqNf.mp3',
-'Xd': 'https://qu.ax/oqNf.mp3',
+'Xd': 'https://qu.ax/MWJz.mp3',
 'Mmm': 'https://qu.ax/YsLt.mp3',
   '@5491168352204|@50231458537': 'https://l.top4top.io/m_2492i4mdu1.mp3'
 }
